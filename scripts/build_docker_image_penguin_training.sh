482f0127d2caad7f4bb6e4dd1bca31e2f3eb73b0
Ubuntu <ubuntu@ip-172-31-10-221.us-west-2.compute.internal>
2020-07-13 13:08:00 +0000

    Separate the shell scripts to their own directory

--- scripts/build_docker_image_penguin_training.sh
+++ scripts/build_docker_image_penguin_training.sh
@@ -1,12 +1,12 @@
 
 # The name of our algorithm
-algorithm_name=coa-gbr
+algorithm_name=penguin-xgb-training
 ls
 account=$(aws sts get-caller-identity --query Account --output text)
 echo $account
 # Get the region defined in the current configuration (default to us-west-1 if none defined)
 region=$(aws configure get region)
-region=${region:-us-west-1}
+region=${region:-us-west-2}
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
 echo $fullname
 # If the repository doesn't exist in ECR, create it.
@@ -21,7 +21,7 @@ $(aws ecr get-login --region ${region} --no-include-email)
 $(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
-cd ~/coa_dev/coa/code/
-docker build -t ${algorithm_name} -f sagemaker_helpers/Dockerfile . --build-arg REGION=${region}
+cd ~/penguin-sagemaker
+docker build -t ${algorithm_name} -f docker/training/Dockerfile . --build-arg REGION=${region}
 docker tag ${algorithm_name} ${fullname}
-docker push ${fullname}
+#docker push ${fullname}
